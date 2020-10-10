9590f1b7becd9fe3a82eb2e761d065aa0f055b90
Brett Fisher <brett.fisher988@gmail.com>
2020-10-10 16:49:26 -0600

    Fix all TS errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,9 @@ import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
 import Image from '../components/image';
-import SEO from '../components/seo';
 
 const IndexPage = (): ReactElement => (
   <Layout>
-    <SEO title="Home" />
     <h1>Brett is Awesome</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
